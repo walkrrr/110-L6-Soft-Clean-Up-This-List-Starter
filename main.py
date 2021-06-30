7688f90539408cdff8fc7743bcf7b19827236fd3
RWR <62685147+walkrrr@users.noreply.github.com>
2021-06-30 04:15:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+websites.pop(2)
+websites.insert(2, "www.lipstickalley.com")
+websites.append("www.skillcrush.com")
+print(websites)
